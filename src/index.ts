d3f631974efea9d36f7d8871f444eb6313d9e629
Sam <jpalmeida0904@gmail.com>
2024-10-07 20:36:08 -0300

    Mutation working
    add:
    - schema.graphql with the schema
    - user interfaces
    - resolver with mutation
    
    change:
    - prisma schema to match User schema
    - '@apollo/server' for 'apollo-server' for  easier management of project

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,40 @@
-import { PrismaClient } from "@prisma/client";
+import { ApolloServer, gql } from "apollo-server";
+import { User } from './interfaces';
+import { readFileSync } from "fs";
+const typeDefs = gql(readFileSync("./src/schema.graphql", "utf8"))
 
-const prisma = new PrismaClient();
+const users: User[] = [];
 
-async function main(){
-    /* await prisma.user.create({
-        data: {
-            name: 'Sam',
-            email: 'jpalmeida0904@gmail.com',
-            username: 'sam0904',
-        },
-    }) */
+const resolvers = {
+    Query: {
+        test: () => "it's working!",
+        users: () => users.map(({ password, ...user }) => user),
+    },
+    Mutation: {
+        createUser: (_, { data }: { data: Omit<User, 'id'> }) => {
+            if(!data){
+                throw new Error("Data is missing!");
+            }
+            const newUser = {
+                id: Math.floor(Math.random() * 100),
+                ...data,
+            };
+            users.push(newUser);
+            return {
+                id: newUser.id,
+                name: newUser.name,
+                email: newUser.email,
+                birthDate: newUser.birthDate,
+            };
+        }
+    }
+};
 
-    /* await prisma.user.update({
-        where: { id: 1 },
-        data: {
-            email: 'salmeida0904@gmail.com',
-        },
-    }) */
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
-    const allUsers = await prisma.user.findMany();
-    console.log(allUsers);
-    
-}
-
-main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1);
-    })
+server.listen().then( async ({ url }) => {
+    console.log(url);
+});
