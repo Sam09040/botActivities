54d0f5e6c7c944f17fda4c26dede67ee91c0621a
Sam <jpalmeida0904@gmail.com>
2024-10-08 15:40:21 -0300

    Working user add to bd
    ## add:
    - password hash
    - prisma client functions
    - many users and user by id queries

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,62 @@
 import { ApolloServer, gql } from "apollo-server";
-import { User } from './interfaces';
+import { UserInput } from './interfaces';
 import { readFileSync } from "fs";
-const typeDefs = gql(readFileSync("./src/schema.graphql", "utf8"))
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+const typeDefs = gql(readFileSync("./src/schema.graphql", "utf8"));
 
-const users: User[] = [];
+const prisma = new PrismaClient();
 
 const resolvers = {
     Query: {
         test: () => "it's working!",
-        users: () => users.map(({ password, ...user }) => user),
+        users: async () => {
+            const users = await prisma.user.findMany();
+            
+            if(!users){
+                return null;
+            }
+
+            return users;
+        },
+        user: async (_: unknown, { id }: { id: number }) => {
+            const user = prisma.user.findUnique({
+                where: { id: id },
+            });
+
+            if(!user){
+                throw new Error("User not found!");
+            }
+            
+            return user;
+        },
     },
     Mutation: {
-        createUser: (_, { data }: { data: Omit<User, 'id'> }) => {
+        createUser: async (_: unknown, { data }: UserInput) => {
             if(!data){
                 throw new Error("Data is missing!");
             }
-            const newUser = {
-                id: Math.floor(Math.random() * 100),
-                ...data,
-            };
-            users.push(newUser);
+
+            const { name, email, password, birthDate } = data;
+
+            const existingEmail = await prisma.user.findFirst({
+                where: { email: email },
+            });
+
+            if(existingEmail){
+                throw new Error("Email already exists!");
+            }
+
+            const saltRounds = 10;
+            const hashPassword = await bcrypt.hash(password, saltRounds);
+            const newUser = await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    password: hashPassword,
+                    birthDate,
+                },
+            });
             return {
                 id: newUser.id,
                 name: newUser.name,
