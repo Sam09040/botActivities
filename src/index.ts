fd3e2b891f774f91c6e17dd1829950314cd3a6fe
Sam <jpalmeida0904@gmail.com>
2024-10-09 12:23:59 -0300

    test setup
    ## add:
    - test\index.mjs file and basic code
    - test script

--- src/index.ts
+++ src/index.ts
@@ -1,87 +1,87 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql } from 'apollo-server';
 import { UserInput } from './interfaces';
-import { readFileSync } from "fs";
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
-const typeDefs = gql(readFileSync("./src/schema.graphql", "utf8"));
+import { readFileSync } from 'fs';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+const typeDefs = gql(readFileSync('./src/schema.graphql', 'utf8'));
 
 const prisma = new PrismaClient();
 
 const isPasswordValid = (password: string): boolean => {
-    const minLength = 6;
-    const hasLetter = /[a-zA-Z]/.test(password);
-    const hasDigit = /\d/.test(password);
+  const minLength = 6;
+  const hasLetter = /[a-zA-Z]/.test(password);
+  const hasDigit = /\d/.test(password);
 
-    return password.length >= minLength && hasLetter && hasDigit;
+  return password.length >= minLength && hasLetter && hasDigit;
 };
 
 const resolvers = {
-    Query: {
-        test: () => "it's working!",
-        users: async () => {
-            const users = await prisma.user.findMany();
-            
-            if(!users){
-                return null;
-            }
+  Query: {
+    test: () => "it's working!",
+    users: async () => {
+      const users = await prisma.user.findMany();
 
-            return users;
-        },
-        user: async (_: unknown, { id }: { id: number }) => {
-            const user = prisma.user.findUnique({
-                where: { id: id },
-            });
+      if (!users) {
+        return null;
+      }
 
-            if(!user){
-                throw new Error("User not found!");
-            }
-            
-            return user;
-        },
+      return users;
     },
-    Mutation: {
-        createUser: async (_: unknown, { data }: UserInput) => {
-            if(!data){
-                throw new Error("Data is missing!");
-            }
+    user: async (_: unknown, { id }: { id: number }) => {
+      const user = prisma.user.findUnique({
+        where: { id: id },
+      });
 
-            const { name, email, password, birthDate } = data;
+      if (!user) {
+        throw new Error('User not found!');
+      }
 
-            const existingEmail = await prisma.user.findFirst({
-                where: { email: email },
-            });
+      return user;
+    },
+  },
+  Mutation: {
+    createUser: async (_: unknown, { data }: UserInput) => {
+      if (!data) {
+        throw new Error('Data is missing!');
+      }
 
-            if(existingEmail){
-                throw new Error("Email already exists!");
-            }
+      const { name, email, password, birthDate } = data;
 
-            if(!isPasswordValid(password)){
-                throw new Error("Password must be at least 6 characters long, have a least one letter and one digit!")
-            }
+      const existingEmail = await prisma.user.findFirst({
+        where: { email: email },
+      });
 
-            const newUser = await prisma.user.create({
-                data: {
-                    name,
-                    email,
-                    password: await bcrypt.hash(password, 10),
-                    birthDate,
-                },
-            });
-            return {
-                id: newUser.id,
-                name: newUser.name,
-                email: newUser.email,
-                birthDate: newUser.birthDate,
-            };
-        }
-    }
+      if (existingEmail) {
+        throw new Error('Email already exists!');
+      }
+
+      if (!isPasswordValid(password)) {
+        throw new Error('Password must be at least 6 characters long, have a least one letter and one digit!');
+      }
+
+      const newUser = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: await bcrypt.hash(password, 10),
+          birthDate,
+        },
+      });
+      return {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        birthDate: newUser.birthDate,
+      };
+    },
+  },
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 
-server.listen().then( async ({ url }) => {
-    console.log(url);
+server.listen().then(async ({ url }) => {
+  console.log(url);
 });
