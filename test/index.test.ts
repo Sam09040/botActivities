6124e24a1b19755a34e35d69bda9b358e5ba54ad
Sam <jpalmeida0904@gmail.com>
2024-11-06 10:43:21 -0300

    small changes
    - tsimp to run .ts
    - run configuration with tsimp
    
    - test file from .mjs to .ts
    - resolvers import on index.ts
    
    - .prettierrc.ts file as it was a duplicate

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,41 +1,90 @@
+/* eslint-disable no-undef */
 import { expect } from 'chai';
 import axios from 'axios';
-import server from '../src/graphql/server';
+import server from '../src/index';
+import { PrismaClient } from '@prisma/client';
+let prisma = new PrismaClient();
+import 'dotenv/config'
 
-before(async () => {
-  try {
-    await server.listen().then(async ({ url }) => {
-      console.log(url);
+describe('createUser mutation', () => {
+    let serverInstance;
+    const port = process.env.PORT;
+    
+    before(async () => {
+        serverInstance = server;
+        if(!server.listen()){
+            server.listen(port).then( async ({ url }) => {
+                console.log(url);
+            })       
+        }
+        if(!prisma.$connect()){
+            await prisma.$connect();
+            console.log(`Connected to database testdb`);
+        }
+        console.log(`Connected to database testdb`);
+        console.log(`Server started on port ${port}`);
     });
-  } catch (error) {
-    if(error instanceof Error) {
-      console.error(`Error starting server: ${error.message}`);
-    }
-  }
-});
+    
+    afterEach(async () => {
+        await prisma.user.deleteMany();
+    });
+    
+    after(async () => {
+        serverInstance = server;
+        if(serverInstance){
+            await serverInstance.stop();
+            serverInstance = null;
+            console.log('Server stopped');
+        }
+        if(prisma) {
+            await prisma.$disconnect();
+            console.log('Database testdb disconnected');
+        }
+    });
 
-after(async () => {
-    if(server){
-        await server.stop();   
-        console.log('Server stopped');
-    }
-});
-
-describe('Apollo Server', () => {
-    it('should respond to hello query', async () => {
-        const query = `
-            query{
-                hello
+    it('should create an user successfully', async () => {
+        const createUser = `
+            mutation createUser($data: UserInput!){
+                createUser(data: $data) {
+                    id,
+                    name,
+                    email,
+                    birthDate
+                }
             }
         `;
 
+        const variables = {
+            data: {
+                name: 'Sam',
+                email: 'sam@example.com',
+                password: 'sam123',
+                birthDate: '2004-04-09'
+            }
+        };
+
         console.log("sending query...");
-        const response = await axios.post('http://localhost:4000/', {
-            query,
+        const response = await axios.post(`http://localhost:${port}/`, {
+            query: createUser,
+            variables,
         });
-        console.log('response received: ', response.data);
+        const { data } = response.data;
+        console.log('response received: ', data);
+
         
 
-        expect(response.data.data.hello).to.equal('hello!');
+        expect(data).to.have.property('createUser');
+        expect(data.createUser).to.have.property('id');
+        expect(data.createUser.name).to.equal('Sam');
+        expect(data.createUser.email).to.equal('sam@example.com');
+        expect(data.createUser.birthDate).to.equal('2004-04-09');
+
+        const userInDb = await prisma.user.findUnique({
+            where: { email: 'sam@example.com' },
+        });
+
+        expect(userInDb).to.not.equal(null);
+        expect(userInDb?.name).to.equal('Sam');
+        console.log(userInDb?.name);
     });
 });
